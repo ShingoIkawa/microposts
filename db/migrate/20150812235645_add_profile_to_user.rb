d5541c7cc2cc0e80d1aaa32abc701ea9dc2642da
shingoikawa <iforse@me.com>
2015-08-13 22:27:22 +0000

    implementation of edit

--- db/migrate/20150812235645_add_profile_to_user.rb
+++ db/migrate/20150812235645_add_profile_to_user.rb
@@ -1,15 +1,6 @@
 class AddProfileToUser < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :area
-      t.string :body
-      t.string :email
-      t.string :password_digest
-
-      t.timestamps
-      
-      t.index :email, unique: true
-    end
+   add_column:users,:area,:string
+   add_column:users,:body,:string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,8 @@ ActiveRecord::Schema.define(version: 20150812235645) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "profile"
+    t.string   "area"
+    t.string   "body"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
